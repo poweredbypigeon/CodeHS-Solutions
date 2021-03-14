d18807ff194a1107217badc6472a579d58384a55
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-03-13 19:57:55 -0500

    Create Our First Integer.java

---
+++ Chapter 2- Basic Java/2.2- Variables and Types/Our First Integer.java
@@ -0,0 +1,11 @@
+public class Variables extends ConsoleProgram
+{
+
+    public void run()
+    {
+        int year = 2021;
+        System.out.println("The current year is " + year);
+        // your code here
+    }
+
+}
