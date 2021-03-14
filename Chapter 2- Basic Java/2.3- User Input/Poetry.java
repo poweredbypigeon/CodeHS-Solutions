ae05c4b31c171d46e1dbf71421b1c076ced217d4
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-03-13 19:43:53 -0500

    Create Poetry.java

---
+++ Chapter 2- Basic Java/2.3- User Input/Poetry.java
@@ -0,0 +1,16 @@
+public class Poetry extends ConsoleProgram
+{
+    public void run()
+    {
+        System.out.println("Here's a parody");
+        System.out.println("Of all modern poetry");
+        System.out.println("I ran out of space");
+        System.out.println("Why does this have to be five lines long?");
+        System.out.println("OK, this poem is kind of awkward.");
+        System.out.println("Really, I'm just doing this to please the autograder.");
+        System.out.println("Do I have to say more? Apparently I do.");
+        System.out.println("So this acrostic was supposed to be about words...");
+        String x = readLine("Input your own poem: ");
+        System.out.println(x);
+    }
+}
