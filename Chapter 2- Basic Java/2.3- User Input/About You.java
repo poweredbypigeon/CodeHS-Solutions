f86d34dbf3cee538b42dc18904acc8c5e5ee19fe
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-03-13 19:47:49 -0500

    Create About You.java

---
+++ Chapter 2- Basic Java/2.3- User Input/About You.java
@@ -0,0 +1,15 @@
+public class AboutYou extends ConsoleProgram
+{
+    public void run()
+    {
+        String food = readLine("What is your favorite food?");
+        String color = readLine("What is your favorite color?");
+        String movie = readLine("What is your favorite movie?");
+        System.out.println(food);
+        System.out.println(color);
+        System.out.println(movie);
+    }
+}
+/* 
+Tip: Use copy and paste from the "Assignment" description.
+*/
