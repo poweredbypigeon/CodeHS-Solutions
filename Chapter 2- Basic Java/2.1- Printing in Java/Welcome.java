6ede47ea6405c105fc21ebce8235c4b9b936e0f6
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-03-13 20:01:17 -0500

    Create Welcome.java

---
+++ Chapter 2- Basic Java/2.1- Printing in Java/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome extends ConsoleProgram
+{
+    public void run()
+    {
+        System.out.println("My name is not Jeremy.");
+        System.out.println("I like anything but comedy.");
+    } 
+}
