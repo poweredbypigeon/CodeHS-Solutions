0925662194f157e220126a35450f1e973c404d52
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-03-13 20:00:43 -0500

    Create ASCII Art.java

---
+++ Chapter 2- Basic Java/2.1- Printing in Java/ASCII Art.java
@@ -0,0 +1,20 @@
+public class Art extends ConsoleProgram{
+    // Copy and paste helps here.
+    public void run()
+    {
+        System.out.println("   /)___");
+        System.out.println("  /     O|___");
+        System.out.println(" /           )");
+        System.out.println("/       )___/");
+    }
+
+}
+/* 
+
+Copied and pasted from the CodeHS Assignment page:
+
+   /)___
+  /     O|___
+ /           )
+/       )___/
+*/
