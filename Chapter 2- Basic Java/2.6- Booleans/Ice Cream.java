c477d4be5e9f1d452d4c3ead1b4c971c012ef05c
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-03-14 10:37:04 -0400

    Create Ice Cream.java

---
+++ Chapter 2- Basic Java/2.6- Booleans/Ice Cream.java
@@ -0,0 +1,9 @@
+public class IceCream extends ConsoleProgram
+{
+    public void run()
+    {
+        boolean x = readBoolean("Do you like ice cream?");
+        System.out.println(x);
+        // Write your code here.
+    }
+}
