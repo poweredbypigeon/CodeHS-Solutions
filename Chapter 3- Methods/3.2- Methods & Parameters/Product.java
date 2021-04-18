1a59c071120eef16ea7a7cbabbcafbb052fdb8c4
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-04-18 16:05:48 -0400

    Create Product.java

---
+++ Chapter 3- Methods/3.2- Methods & Parameters/Product.java
@@ -0,0 +1,16 @@
+public class Multiply extends ConsoleProgram
+{
+    public void run()
+    {
+        multiply(2, 20);
+        multiply(5, 19);
+        multiply(100, 15);
+        
+    }
+    
+    private void multiply(int a, int b) {
+        System.out.println(a*b);
+        // Your code here. 
+        // Print out the product of a and b.
+    }
+}
