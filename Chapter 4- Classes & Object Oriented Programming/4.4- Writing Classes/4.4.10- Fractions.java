779fde29c1fa14fa67ae0ecb7500641d2e163b72
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-06-20 08:22:53 -0400

    Create 4.4.10- Fractions.java

---
+++ Chapter 4- Classes & Object Oriented Programming/4.4- Writing Classes/4.4.10- Fractions.java
@@ -0,0 +1,12 @@
+public class Fraction
+{
+      private int num;
+      private int dem;
+      public Fraction(int n1, int n2) {
+          num = n1;
+          dem = n2;
+      }
+      public String toString() {
+          return num + "/" + dem;
+      }
+}
