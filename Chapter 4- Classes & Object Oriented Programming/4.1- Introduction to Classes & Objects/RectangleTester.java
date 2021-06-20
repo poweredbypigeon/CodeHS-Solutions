05e82574416de2cd1911a5240feb898a2b624e00
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-06-19 21:03:47 -0400

    Create RectangleTester.java

---
+++ Chapter 4- Classes & Object Oriented Programming/4.1- Introduction to Classes & Objects/RectangleTester.java
@@ -0,0 +1,10 @@
+public class RectangleTester extends ConsoleProgram
+{
+    public void run()
+    {
+        // Create a rectangle with width 5 and height 12
+        Rectangle r = new Rectangle(5, 12);
+        // Then print it out
+        System.out.println(r);
+    }
+}
