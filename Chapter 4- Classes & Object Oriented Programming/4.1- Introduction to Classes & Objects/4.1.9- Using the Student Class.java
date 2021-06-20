97f95a4611573848d6aa3ed2d0b78ea7ab504a27
ThisUsernameIs-Taken <75860945+ThisUsernameIs-Taken@users.noreply.github.com>
2021-06-20 08:15:03 -0400

    Create 4.1.9- Using the Student Class.java

---
+++ Chapter 4- Classes & Object Oriented Programming/4.1- Introduction to Classes & Objects/4.1.9- Using the Student Class.java
@@ -0,0 +1,16 @@
+public class StudentTester extends ConsoleProgram
+{
+    public void run()
+    {
+        Student alan = new Student("Alan", "Turing", 11);
+        Student ada = new Student("Ada", "Lovelace", 12);
+        Student kid = new Student("Kid", "McPerson", 21000000);
+        // Add an entry for yourself here!
+        
+        System.out.println(alan);
+        System.out.println(ada);
+        System.out.println(kid);
+        // Print out your student object
+
+    }
+}
